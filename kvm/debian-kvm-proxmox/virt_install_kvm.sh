e2f74e79733d48c94586bd90433c50f307aded04
reda <redaakherfi07@gmail.com>
2024-05-25 12:58:56 +0100

    update

--- kvm/debian-kvm-proxmox/virt_install_kvm.sh
+++ kvm/debian-kvm-proxmox/virt_install_kvm.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+VMNAME=deepspace
+DOMAIN=reda-boss
+NETWORK=default
+OSVARIANT=debian12
 # source the colors script
 # create the prompting function
 
-sudo virt-install --connect qemu:///system \
-    --
+virt-install --connect=qemu:///system --name=${VMNAME} --ram=1024 --vcpus=2 \
+    --disk path=/var/lib/libvirt/images/${VMNAME}.qcow2,size=8,bus=virtio,format=qcow2 \
+    --initrd-inject=/var/lib/libvirt/images/preseeds/bookworm/preseed.cfg \
+    --location http://ftp.de.debian.org/debian/dists/bookworm/main/installer-amd64 \
+    --graphics none \
+    --os-variant ${OSVARIANT}  --network network=default \
+    --extra-args="auto=true hostname="${VMNAME}" domain="${DOMAIN}.local" console=tty0 console=ttyS0,115200n8 serial";
--- kvm/virtinstall_debian.sh
+++ kvm/virtinstall_debian.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+virsh destroy --domain archer
+virsh undefine --nvram archer
+rm /var/lib/libvirt/images/archer*
 
 
-sudo virt-install    --connect qemu:///system --name debian-script-02     --memory memory=1024,currentMemory=512         --vcpus maxvcpus=7,vcpus=1                     --cpu   mode=host-passthrough                --network  network=default       --location /var/lib/libvirt/isos/debian-12.5.0-amd64-DVD-1.iso        --os-variant debian12                 --disk size=5,format=qcow2,cache=none,discard=unmap        --tpm default       --channel type=unix,target.type=virtio,target.name=org.qemu.guest_agent.0      --graphics none     --extra-args 'console=tty0 console=ttyS0,115200n8 --- console console=tty0 console=ttyS0,115200n8'     --boot uefi 
+sudo virt-install    --connect qemu:///system --name archer   --disk size=20,format=qcow2,cache=none,discard=unmap      --memory memory=1024,currentMemory=1024         --vcpus maxvcpus=7,vcpus=1                     --cpu   mode=host-passthrough                --network  network=default  --cdrom /var/lib/libvirt/isos/archlinux-2024.05.01-x86_64.iso    --os-variant archlinux            --tpm default       --channel type=unix,target.type=virtio,target.name=org.qemu.guest_agent.0      --graphics spice    --boot uefi 
 
-
+sleep 5
+virt-viewer --connect=$(virsh uri) --attach archer
