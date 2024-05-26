915f3aa175958f8752082bb646b271e2bd251b2d
reda <redaakherfi07@gmail.com>
2024-05-26 17:33:34 +0100

    update

--- nfs/server/installing_server.sh
+++ nfs/server/installing_server.sh
@@ -16,5 +16,17 @@ case $DISTRO in
         sudo apt update -y
         sudo apt upgrade -y
         sudo apt install nfs-kernel-server -y
+        sudo systemctl enable --now nfs-kernel-server
+        # making the share/export
+        sudo mkdir -p /home/reda/breen
+        sudo chown nobody:nogroup /home/reda/breen
+        sudo chmod 755 /home/reda/breen
+        sudo echo "/home/reda/breen 192.168.1.0/24(rw,sync,no_subtree_check)" >> /etc/exports
+        sudo exportfs -a
+        # firewall stuff
+        sudo apt install ufw -y
+        sudo ufw enable
+        sudo ufw allow from 192.168.1.0/24 to any port nfs
+        sudo ufw reload
         ;;
 esac
