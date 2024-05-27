fefbd51cc337615d187ecf590631ec7125a53a23
reda <redaakherfi07@gmail.com>
2024-05-27 18:11:16 +0100

    update

--- debian_initial_setup.sh
+++ debian_initial_setup.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# the /etc/sources.list file
+# XXX remove the CD rom one
+# deb http://deb.debian.org/debian bookworm main contrib non-free-firmware non-free
+# deb http://deb.debian.org/debian bookworm-updates main contrib non-free-firmware non-free
+# deb http://security.debian.org/debian-security bookworm-security main contrib non-free-firmware non-free
+########################################
 
 # run as root
 apt update -y
